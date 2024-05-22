1a7deb8248cea2ae57b21c550aabf31d76eb3940
Justintimefordinner <justwright916@gmail.com>
2024-05-21 21:36:31 -0500

    computerswap

--- GUAC.py
+++ GUAC.py
@@ -2,17 +2,21 @@ from Bio.Seq import Seq
 import random
 import math
 import pprint
+import uuid
 
 class player:
-    def __init__(self, playerID: int, ip):
+    def __init__(self, playerID: int, ip, uname):
         self.playerID = playerID
         self.ip = ip
+        self.uname = uname
         #self.mac = MAC
         self.score = 0
         self.multiplier = 1
         self.combo = 0
+        self.baseNo = 0
         
     def update_score(self, score: bool):
+        self.baseNo += 1
         if score:
             self.combo += 1
             self.score += 1 * self.multiplier
@@ -33,6 +37,15 @@ class GUAC:
         self.scoreboard = {}
         self.players = {}
         self.game_over = False
+        
+    def id_gen(self):
+            """
+            Generate a unique identifier.
+
+            Returns:
+            - str: The generated unique identifier.
+            """
+            return str(uuid.uuid4())
     
     def add_player(self, uname, ip):
         """
@@ -42,14 +55,52 @@ class GUAC:
         - uname (str): The username of the player.
         - ip (str): The IP address of the player.
         """
-        self.players[uname] = player(uname, ip)
-        self.scoreboard[uname] = 0
+        id = self.id_gen()
+        self.players[id] = player(id, ip, uname)
+        self.scoreboard[id] = 0
+        
+    def remove_player(self, playerID):
+        """
+        Remove a player from the game.
+
+        Parameters:
+        - playerID (str): The ID of the player to remove.
+        """
+        del self.players[playerID]
+        del self.scoreboard[playerID]
+        
+    def getID(self, uname):
+        """
+        Get the ID of the player.
+
+        Parameters:
+        - uname (str): The username of the player.
+
+        Returns:
+        - str: The ID of the player.
+        """
+        for player in self.players.values():
+            if player.uname == uname:
+                return player.playerID
+        return None
         
     def generate_sequence(self):
         """
         Generate a random DNA sequence.
         """
         self.sequence = Seq("".join(random.choice("ATGC") for _ in range(20)))
+    
+    def nextbase(self, playerID):
+        """
+        Get the next base in the sequence.
+
+        Parameters:
+        - playerID (str): The ID of the player.
+
+        Returns:
+        - str: The next base in the sequence.
+        """
+        return self.sequence[player.baseNo]
 
     def checker(self, playerID, complement, timestamp):
         """
@@ -63,7 +114,7 @@ class GUAC:
         Returns:
         - bool: True if the complement is valid, False otherwise.
         """
-        if self.sequence[0] == complement:
+        if self.sequence[playerID.baseNo] == complement:
             return True
         return False
     
@@ -75,10 +126,10 @@ class GUAC:
         - dict: The updated scoreboard with player usernames as keys and scores as values.
         """
         for player in self.players.values():
-            self.scoreboard[player.playerID] = player.score
+            self.scoreboard[player.uname()] = player.score
             
         return self.scoreboard
-        
+    
     
     # def convert_to_mRNA(self):
     #     # Convert the DNA sequence to mRNA
--- Server.py
+++ Server.py
@@ -1,6 +1,8 @@
 import socket
 import threading
 from GUAC import GUAC
+import queue
+from time import sleep
 
 # socket instance
 socket_instance = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,23 +11,68 @@ socket_instance.listen(5)
 
 game = GUAC()
 
-def server():
-   while True:
+# Dictionary to store the queues for each player
+player_queues = {}
+
+def game_lobby():
+    while True:
         socket_connection, address = socket_instance.accept()
         uname = socket_connection.recv(1024).decode()
         print(f'{uname} connected')
         game.add_player(uname, address)
-        threading.Thread(target=handle_user_connection, args=[socket_connection, address]).start()
+        
+        # Create a new queue for the player
+        player_queues[game.getID(uname)] = queue.Queue()
+        
+        threading.Thread(target=handle_user_connection, args=[socket_connection, address, game.getID(uname)]).start()
+        
+        # Display the current players in the lobby
+        print('Current players in the lobby:')
+        for player in game.players:
+            print(player)
+        
+        # Send a start signal to all connected players
+        for player_queue in player_queues.values():
+            player_queue.put('start')
 
-def response_manager(client):
-   while True:
-       message = client.recv(1024)
-       print('message: ', message.decode())
-       message = input('=> ')
-       client.send(message.encode())
+def server():
+    # Start the game lobby in a separate thread
+    threading.Thread(target=game_lobby).start()
 
-def handle_user_connection(connection: socket.socket, address: str) -> None:
-    pass
+    while True:
+        sleep(1)  # Sleep to reduce CPU usage
+
+
+def handle_user_connection(client: socket.socket, address: str, id: int) -> None:
+    """
+    Function to handle the server-side interaction.
+
+    Parameters:
+    connection (socket.socket): The connection object.
+    address (str): The address of the client.
+
+    Returns:
+    None
+    """
+    while True:
+        try:
+            message = client.recv(1024).decode()
+        except Exception:
+            print(f'Player {id} disconnected')
+            game.remove_player(id)
+            del player_queues[id]
+            break
+
+        player_id, letter, timestamp = message.split(',')
+        
+        # Put the message in the player's queue
+        player_queues[player_id].put((letter, timestamp))
+        
+        # Get the next message from the player's queue
+        next_letter, next_timestamp = player_queues[player_id].get()
+        
+        game.checker(player_id, next_letter, next_timestamp)
+        client.send(game.nextbase(player_id).encode())
 
 server()
 # serverside - loads dna strand
